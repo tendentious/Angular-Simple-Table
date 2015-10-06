4b6a0f098793355c54ffbc9a98b7e46554251fe3
Andrei <andrei_pericleanu@yahoo.com>
2015-10-06 14:26:17 +0300

    Data display and update

--- Vendor/js/angular-simple-table.js
+++ Vendor/js/angular-simple-table.js
@@ -1,34 +1,73 @@
 angular.module("angular-simple-table",[])
-//	.directive('ngSparkline',function(){
-//            console.log(1);
-////        var link = function(scope, iElement, iAttrs, ctrl){
-////            console.log(1);
-////        };
-//
-//return {
-//    restrict: 'A',
-//    template: '<div class="sparkline"></div>'
-//  };
-//        return{
-////            transclude:true,
-////            link:link,
-//            restrict:'EA',
-//            scope:true
-//        };
-//    });
 
-    .directive('simpleTable', function($compile) {
-        var link = function(scope, iElement, iAttrs,nullController, transclude){
-            transclude(function(clone){
-                iElement.append(clone);
-            });
+.directive('simpleTable',['$filter', function($filter) {
+    window.TableSettings = function (data){
+        this.data = data;
+        var itemsPerPage = 25;
+        var currentPage = 1;
+        this.setItemsPerPage = function(limit){ itemsPerPage = limit; };
+        this.getItemsPerPage = function(){return itemsPerPage;};
+        this.getCurrentPage = function(){return currentPage;};
+        this.setCurrentPage = function(nr){currentPage = nr;};
+        this.setData = function(data){ this.data = data; };
+        this.getData = function(){
+            var filteredData = data;
+            //filter Current Page and Number of Items Per Page
+            var startIndex = (currentPage - 1) * itemsPerPage;
+            filteredData = filteredData.slice(startIndex,startIndex + itemsPerPage);
+            return filteredData;
         };
+    };
 
-        return {
-            transclude:true,
-            scope:true,
-            restrict: 'EA',
-            template: '<div class="sparkline"></div>',
-            link: link
-        };
-    });
+    var link = function($scope, iElement, iAttrs,nullController, transclude){
+        //assign the table to directive's isolated scope
+        var table = document.querySelectorAll('[data-table-settings]');
+        for(var item in table){
+            if(table.hasOwnProperty(item)){
+                var currentItem = table[item];
+                if(iAttrs['tableSettings'] === currentItem.getAttribute("data-table-settings") && currentItem.tagName === "TABLE"){
+                    $scope.table = table[item];
+                };
+            }
+        }
+        //add thead
+        iElement.prepend(document.createElement("tHead"));
+
+        //watch for data change
+        $scope.$watch(
+            function(){
+                var row = $scope.table.children[1].children[0];
+                return row;
+            },
+            function(newValue,oldValue){
+                //if table rows appear for the first time
+                if(typeof newValue !== 'undefined'){
+                    var row = newValue;
+
+                     //select tr from thead
+                    var headRow = $scope.table.children[0];
+                    headRow.innerHTML='';
+                    allTd = row.children;
+                    var tr = document.createElement("tr");
+                    for(var item in allTd){
+                        if(allTd.hasOwnProperty(item)){
+                            var th = document.createElement("th");
+                            th.innerHTML = allTd[item].getAttribute("title");
+                            tr.appendChild(th);
+                        }
+                    }
+                    headRow.appendChild(tr);
+                }
+        });
+    };
+
+    return {
+        scope:{
+                tableSettings: "&"
+        },
+        priority:1001,
+        restrict: 'EA',
+        template: '',
+        link: link,
+    };
+}]);
--- Vendor/js/myAngularApplication.js
+++ Vendor/js/myAngularApplication.js
@@ -1,15 +1,42 @@
 angular.module("myApp",["angular-simple-table"]).
 
-    controller('main-controller',["$scope",function($scope){
-        $scope.users = [{"name":"John","country":"Romania","age":22},
-            {"name":"Mary","country":"Germany","age":24},
-            {"name":"Alex","country":"France","age":34},
-            {"name":"Frank","country":"U.S.","age":65},
-            {"name":"Mario","country":"Italy","age":40}
+    controller('main-controller',["$scope",'$timeout',function($scope,$timeout){
+        $scope.users = [
+            {"id":1,"name":"John","country":"Romania","age":22},
+            {"id":2,"name":"Mary","country":"Germany","age":24},
+            {"id":3,"name":"Alex","country":"France","age":34},
+            {"id":4,"name":"Frank","country":"U.S.","age":65},
+            {"id":5,"name":"Mario","country":"Italy","age":40}
         ];
-        $scope.test = [{"first":1,"second":2,"third":3},
+
+            $scope.test = [{"first":1,"second":2,"third":3},
             {"first":41,"second":42,"third":43},
             {"first":21,"second":22,"third":23}
-        ];
+                ];
+
+        //$timeout(function(){
+        //    $scope.myTableSettings2.setCurrentPage(2);
+        //    //$scope.$digest();
+        //},2000);
+        //setInterval(function(){
+        //    console.log("interval");
+        //    if($scope.myTableSettings.data == false){
+        //        $scope.myTableSettings.data = $scope.test;
+        //        $scope.myTableSettings2.data = $scope.users;
+        //        $scope.$digest();}
+        //    else{
+        //        $scope.myTableSettings.data = false;
+        //        $scope.myTableSettings2.data = false;
+        //        $scope.$digest();
+        //    }
+        //},1000);
+
         console.log("main");
+        $scope.myTableSettings = new TableSettings($scope.test);
+        $scope.myTableSettings2 = new TableSettings($scope.users);
+        $scope.myTableSettings2.setItemsPerPage(3);
+        $scope.myTableSettings2.setCurrentPage(1);
+        //$scope.myTableSettings = new TableSettings(1);
+        //$scope.myTableSettings2 = new TableSettings(2);
+
     }]);
